c96bc4b53842770a64e129cc49ff29824378b1a8
krouw <felipe.andrez@live.cl>
2017-07-19 01:33:48 -0400

    Add flask logicFuzzy

--- GPI.py
+++ GPI.py
@@ -6,101 +6,100 @@ import os
 
 # Agregar antecedente(s) que contienen el universo de variales
 # y funciones de pertenencia
+def fuzzyLogic(body):
+    habilidad1 = ctrl.Antecedent(np.arange(0,11.0,1), 'habilidad1')
+    habilidad1['armonico'] = fuzz.trapmf(habilidad1.universe, [0.0, 0.0, 2.0, 2.1])
+    habilidad1['analitico'] = fuzz.trapmf(habilidad1.universe, [2.0, 2.1, 4.0, 4.1])
+    habilidad1['comunicador'] = fuzz.trapmf(habilidad1.universe, [4.0, 4.1, 6.0, 6.1])
+    habilidad1['competitivo'] = fuzz.trapmf(habilidad1.universe, [6.0, 6.1, 8.0, 8.1])
+    habilidad1['disciplinado'] = fuzz.trapmf(habilidad1.universe, [8.0, 8.1, 10.0, 10.0])
 
-habilidad1 = ctrl.Antecedent(np.arange(0,11.0,1), 'habilidad1')
-habilidad1['armonico'] = fuzz.trapmf(habilidad1.universe, [0.0, 0.0, 2.0, 2.1])
-habilidad1['analitico'] = fuzz.trapmf(habilidad1.universe, [2.0, 2.1, 4.0, 4.1])
-habilidad1['comunicador'] = fuzz.trapmf(habilidad1.universe, [4.0, 4.1, 6.0, 6.1])
-habilidad1['competitivo'] = fuzz.trapmf(habilidad1.universe, [6.0, 6.1, 8.0, 8.1])
-habilidad1['disciplinado'] = fuzz.trapmf(habilidad1.universe, [8.0, 8.1, 10.0, 10.0])
+    habilidad2 = ctrl.Antecedent(np.arange(0,11.0,1), 'habilidad2')
+    habilidad2['empatico'] = fuzz.trapmf(habilidad2.universe, [0.0, 0.0, 2.0, 2.1])
+    habilidad2['flexible'] = fuzz.trapmf(habilidad2.universe, [2.0, 2.1, 4.0, 4.1])
+    habilidad2['responsable'] = fuzz.trapmf(habilidad2.universe, [4.0, 4.1, 6.0, 6.1])
+    habilidad2['emprendedor'] = fuzz.trapmf(habilidad2.universe, [6.0, 6.1, 8.0, 8.1])
+    habilidad2['mandatario'] = fuzz.trapmf(habilidad2.universe, [8.0, 8.1, 10.0, 10.0])
 
-habilidad2 = ctrl.Antecedent(np.arange(0,11.0,1), 'habilidad2')
-habilidad2['empatico'] = fuzz.trapmf(habilidad2.universe, [0.0, 0.0, 2.0, 2.1])
-habilidad2['flexible'] = fuzz.trapmf(habilidad2.universe, [2.0, 2.1, 4.0, 4.1])
-habilidad2['responsable'] = fuzz.trapmf(habilidad2.universe, [4.0, 4.1, 6.0, 6.1])
-habilidad2['emprendedor'] = fuzz.trapmf(habilidad2.universe, [6.0, 6.1, 8.0, 8.1])
-habilidad2['mandatario'] = fuzz.trapmf(habilidad2.universe, [8.0, 8.1, 10.0, 10.0])
+    competencia = ctrl.Antecedent(np.arange(0, 11.0, 1), 'competencia')
+    competencia['basicas'] = fuzz.trapmf(competencia.universe, [0.0, 0.0, 2.0, 2.1])
+    competencia['comportamiento'] = fuzz.trapmf(competencia.universe, [2.0, 2.1, 4.0, 4.1])
+    competencia['tecnicas'] = fuzz.trapmf(competencia.universe, [4.0, 4.1, 6.0, 6.1])
+    competencia['intelectuales'] = fuzz.trapmf(competencia.universe, [6.0, 6.1, 8.0, 8.1])
+    competencia['orden y seguridad'] = fuzz.trapmf(competencia.universe, [8.0, 8.1, 10.0, 10.0])
 
-competencia = ctrl.Antecedent(np.arange(0, 11.0, 1), 'competencia')
-competencia['basicas'] = fuzz.trapmf(competencia.universe, [0.0, 0.0, 2.0, 2.1])
-competencia['comportamiento'] = fuzz.trapmf(competencia.universe, [2.0, 2.1, 4.0, 4.1])
-competencia['tecnicas'] = fuzz.trapmf(competencia.universe, [4.0, 4.1, 6.0, 6.1])
-competencia['intelectuales'] = fuzz.trapmf(competencia.universe, [6.0, 6.1, 8.0, 8.1])
-competencia['orden y seguridad'] = fuzz.trapmf(competencia.universe, [8.0, 8.1, 10.0, 10.0])
+    necesidad1 = ctrl.Antecedent(np.arange(0, 11.0, 1), 'necesidad1')
+    necesidad1['relacionar'] = fuzz.trapmf(necesidad1.universe, [0.0, 0.0, 2.0, 2.1])
+    necesidad1['influir'] = fuzz.trapmf(necesidad1.universe, [2.0, 2.1, 4.0, 4.1])
+    necesidad1['destacarse'] = fuzz.trapmf(necesidad1.universe, [4.0, 4.1, 6.0, 6.1])
+    necesidad1['exito'] = fuzz.trapmf(necesidad1.universe, [6.0, 6.1, 8.0, 8.1])
+    necesidad1['desafios'] = fuzz.trapmf(necesidad1.universe, [8.0, 8.1, 10.0, 10.0])
 
-necesidad1 = ctrl.Antecedent(np.arange(0, 11.0, 1), 'necesidad1')
-necesidad1['relacionar'] = fuzz.trapmf(necesidad1.universe, [0.0, 0.0, 2.0, 2.1])
-necesidad1['influir'] = fuzz.trapmf(necesidad1.universe, [2.0, 2.1, 4.0, 4.1])
-necesidad1['destacarse'] = fuzz.trapmf(necesidad1.universe, [4.0, 4.1, 6.0, 6.1])
-necesidad1['exito'] = fuzz.trapmf(necesidad1.universe, [6.0, 6.1, 8.0, 8.1])
-necesidad1['desafios'] = fuzz.trapmf(necesidad1.universe, [8.0, 8.1, 10.0, 10.0])
+    necesidad2 = ctrl.Antecedent(np.arange(0, 11.0, 1), 'necesidad2')
+    necesidad2['supervision'] = fuzz.trapmf(necesidad2.universe, [0.0, 0.0, 2.0, 2.1])
+    necesidad2['sueldo'] = fuzz.trapmf(necesidad2.universe, [2.0, 2.1, 4.0, 4.1])
+    necesidad2['condiciones'] = fuzz.trapmf(necesidad2.universe, [4.0, 4.1, 6.0, 6.1])
+    necesidad2['politicas'] = fuzz.trapmf(necesidad2.universe, [6.0, 6.1, 8.0, 8.1])
+    necesidad2['administracion'] = fuzz.trapmf(necesidad2.universe, [8.0, 8.1, 10.0, 10.0])
 
-necesidad2 = ctrl.Antecedent(np.arange(0, 11.0, 1), 'necesidad2')
-necesidad2['supervision'] = fuzz.trapmf(necesidad2.universe, [0.0, 0.0, 2.0, 2.1])
-necesidad2['sueldo'] = fuzz.trapmf(necesidad2.universe, [2.0, 2.1, 4.0, 4.1])
-necesidad2['condiciones'] = fuzz.trapmf(necesidad2.universe, [4.0, 4.1, 6.0, 6.1])
-necesidad2['politicas'] = fuzz.trapmf(necesidad2.universe, [6.0, 6.1, 8.0, 8.1])
-necesidad2['administracion'] = fuzz.trapmf(necesidad2.universe, [8.0, 8.1, 10.0, 10.0])
+    # # Funciones de pertenencia para la variable de salida
 
-# # Funciones de pertenencia para la variable de salida
+    salida = ctrl.Consequent(np.arange(0, 11, 1), 'salida')
 
-salida = ctrl.Consequent(np.arange(0, 11, 1), 'salida')
+    salida['cargo1'] = fuzz.trapmf(salida.universe, [0, 0, 2, 2.5])
+    salida['cargo2'] = fuzz.trapmf(salida.universe, [2, 2.5, 4.0, 4.5])
+    salida['cargo3'] = fuzz.trapmf(salida.universe, [4, 4.5, 6.0, 6.5])
+    salida['cargo4'] = fuzz.trapmf(salida.universe, [6, 6.5, 8.0, 8.5])
+    salida['cargo5'] = fuzz.trapmf(salida.universe, [8, 8.5, 10.0, 11.0])
 
-salida['cargo1'] = fuzz.trapmf(salida.universe, [0, 0, 2, 2.5])
-salida['cargo2'] = fuzz.trapmf(salida.universe, [2, 2.5, 4.0, 4.5])
-salida['cargo3'] = fuzz.trapmf(salida.universe, [4, 4.5, 6.0, 6.5])
-salida['cargo4'] = fuzz.trapmf(salida.universe, [6, 6.5, 8.0, 8.5])
-salida['cargo5'] = fuzz.trapmf(salida.universe, [8, 8.5, 10.0, 11.0])
-
-#
-# salida.view()
-# time.sleep(115)
-#
-
-
-##REGLA alta probabilidad de incendio
-rule_cargo1 = ctrl.Rule((habilidad1['armonico'] | habilidad2['empatico'])
-                                          | (competencia['basicas'] | necesidad1['relacionar'])
-                                          | (necesidad2['supervision']), salida['cargo1'])
-
-##REGLA media probabilidad de incendio
-rule_cargo2 = ctrl.Rule((habilidad1['analitico'] | habilidad2['flexible'])
-                                       | (competencia['comportamiento'] | necesidad1['influir'])
-                                       | (necesidad2['sueldo']), salida['cargo2'])
-
-##REGLA baja probabilidad de incendio
-rule_cargo3 = ctrl.Rule((habilidad1['comunicador'] | habilidad2['responsable'])
-                           | (competencia['tecnicas'] | necesidad1['destacarse'])
-                           | (necesidad2['condiciones']), salida['cargo3'])
-
-rule_cargo4 = ctrl.Rule((habilidad1['comunicador'] | habilidad2['responsable'])
-                           | (competencia['tecnicas'] | necesidad1['destacarse'])
-                           | (necesidad2['condiciones']), salida['cargo4'])
-
-rule_cargo5 = ctrl.Rule((habilidad1['comunicador'] | habilidad2['responsable'])
-                           | (competencia['tecnicas'] | necesidad1['destacarse'])
-                           | (necesidad2['condiciones']), salida['cargo5'])
+    #
+    # salida.view()
+    # time.sleep(115)
+    #
 
 
+    ##REGLA alta probabilidad de incendio
+    rule_cargo1 = ctrl.Rule((habilidad1['armonico'] | habilidad2['empatico'])
+                                              | (competencia['basicas'] | necesidad1['relacionar'])
+                                              | (necesidad2['supervision']), salida['cargo1'])
 
-calidad_ctrl = ctrl.ControlSystem([rule_cargo1, rule_cargo2, rule_cargo3, rule_cargo4, rule_cargo5])
-calidad = ctrl.ControlSystemSimulation(calidad_ctrl)
+    ##REGLA media probabilidad de incendio
+    rule_cargo2 = ctrl.Rule((habilidad1['analitico'] | habilidad2['flexible'])
+                                           | (competencia['comportamiento'] | necesidad1['influir'])
+                                           | (necesidad2['sueldo']), salida['cargo2'])
 
-calidad.input['habilidad1'] = 1
-calidad.input['habilidad2'] = 1
-calidad.input['competencia'] = 1
-calidad.input['necesidad1'] = 1
-calidad.input['necesidad2'] = 1
+    ##REGLA baja probabilidad de incendio
+    rule_cargo3 = ctrl.Rule((habilidad1['comunicador'] | habilidad2['responsable'])
+                               | (competencia['tecnicas'] | necesidad1['destacarse'])
+                               | (necesidad2['condiciones']), salida['cargo3'])
+
+    rule_cargo4 = ctrl.Rule((habilidad1['comunicador'] | habilidad2['responsable'])
+                               | (competencia['tecnicas'] | necesidad1['destacarse'])
+                               | (necesidad2['condiciones']), salida['cargo4'])
+
+    rule_cargo5 = ctrl.Rule((habilidad1['comunicador'] | habilidad2['responsable'])
+                               | (competencia['tecnicas'] | necesidad1['destacarse'])
+                               | (necesidad2['condiciones']), salida['cargo5'])
 
 
-calidad.compute()
 
-#
-print "\nResultado"
-print calidad.output['salida']
-print "\n"
+    calidad_ctrl = ctrl.ControlSystem([rule_cargo1, rule_cargo2, rule_cargo3, rule_cargo4, rule_cargo5])
+    calidad = ctrl.ControlSystemSimulation(calidad_ctrl)
+
+    print body['name']
+
+    calidad.input['habilidad1'] = 10.0
+    calidad.input['habilidad2'] = 3.3
+    calidad.input['competencia'] = 1.8
+    calidad.input['necesidad1'] = 2.3
+    calidad.input['necesidad2'] = 1.0
 
 
-salida.view(sim=calidad)
-time.sleep(110)
+    calidad.compute()
 
+    #
+    print "\nResultado"
+    print calidad.output['salida']
+    print "\n"
+
+    return
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # SistemaExpertoGPI
+
+### Instalación
+
+##### 1 Instalar Programas
+  + Instalar Python 2.7
+  + Instalar pip
+  + Instalar gcc
+  + Instalar python-virtualenv
+
+##### 2 Ambiente de Desarrollo
+  + Instalar virtualenv -> sudo pip install virtualenv
+  + En el directorio del proyecto ejecutar _'virtualenv venv'_
+  + Luego ejecutar _'. venv/bin/activate'_
+
+##### 3 Instalación Módulos Python
+  + Instalar numpy -> sudo pip install numpy
+  + Instalar scipy -> sudo pip install scipy
+  + Instalar matplotlib -> sudo pip install matplotlib
+  + Instalar scikit-fuzzy -> sudo pip install scikit-fuzzy
+  + Instalar Flask -> sudo pip install Flask
+  + Instalar flask_restful -> sudo pip install flask_restful
+
+##### 4 Correr
+  + Ejercutar en el directorio del projecto *'FLASK_APP=server.py FLASK_DEBUG=1 python -m flask run'*
